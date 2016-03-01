158c1fbc2a94ad5a3982cf0eb625d87a54ec0090
Fabien Linardon <fabien.linardon@epitech.eu>
2016-02-29 19:13:24 -0800

    MainActivity created (called after a success login) + material NavigationView added with some menu items (notes, shared notes, record notes, settings, friends, logout)

--- app/src/main/java/takeanote/takeanote/activity/LoginActivity.java
+++ app/src/main/java/takeanote/takeanote/activity/LoginActivity.java
@@ -1,8 +1,11 @@
 package takeanote.takeanote.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import takeanote.takeanote.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -11,5 +14,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.btn_connection)
+    void connectionOnClick() {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
 }
